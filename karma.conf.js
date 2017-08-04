a1da164df01304f840524d8e79bb2a2aeacf5f93
Tim Gundmann <tim@gundmann.dk>
2017-08-04 10:49:57 +0200

    karma changed

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,12 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    customLaunchers: {
+    	Chrome_without_gpu: {
+          base: 'Chrome',
+          flags: ['--disable-gpu']
+        }
+      },
+	singleRun : false
   });
 };
